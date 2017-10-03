776406de197697e4a4f214c30f58097ca0c1f7a4
bbalegere <bharatbalegere@gmail.com>
2017-10-03 18:10:29 +0530

    Add support for interviews taking multiple slots

--- InterviewSchedulerGurobi.py
+++ InterviewSchedulerGurobi.py
@@ -20,6 +20,21 @@ def read_input_csv(filename):
     return matrix, row_header, col_header
 
 
+def read_slots_interviews(filename):
+    row_header = list()
+    slots_c = list()
+    with open(filename) as f:
+        for csvline in f:
+            csvline = csvline.strip()
+            if len(row_header) == 0:
+                row_header = csvline.split(',')
+                continue
+
+            slots_c = map(int, csvline.split(','))
+
+    return dict(zip(row_header, slots_c))
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: InterviewScheduler Shortlists.csv SlotsPanels.csv Prefs.csv")
@@ -41,6 +56,21 @@ if __name__ == "__main__":
     assert (sorted(clubs) == sorted(clubs2))
     assert (sorted(clubs) == sorted(clubs3))
 
+    for val in shortlists.values():
+        if val not in [0, 1]:
+            raise ValueError('The shortlists data can have only 0s or 1s indicating whether the student has a shortlist or not')
+
+    for val in panels.values():
+        if not val.is_integer():
+            raise ValueError('The number of panels should be a whole number')
+
+        if val < 0:
+            raise ValueError('THe number of panels cannot be negative')
+
+    slots_int = dict()
+    if len(sys.argv) > 4:
+        slots_int = read_slots_interviews(sys.argv[4])
+
     # Find out max number of panels
     maxpanels = dict((c, max(panels[s, c] for s in slots)) for c in clubs)
 
@@ -57,12 +87,11 @@ if __name__ == "__main__":
     compshortlists = dict((c, sum(shortlists[n, c] for n in names)) for c in clubs)
 
     # Calculate total number of panels per company
-    comppanels = dict((c, sum(panels[s, c] for s in slots)) for c in clubs)
+    comppanels = dict((c, int(sum(panels[s, c] for s in slots) / slots_int.get(c, 1))) for c in clubs)
 
     for c in clubs:
         if compshortlists[c] > comppanels[c]:
-            print(
-                c + " has shortlists greater than no of panels " + str(compshortlists[c]) + " > " + str(comppanels[c]))
+            print(c + " has shortlists greater than no of panels " + str(compshortlists[c]) + " > " + str(comppanels[c]))
 
     # Create Objective Coefficients
     prefsnew = dict()
@@ -75,7 +104,7 @@ if __name__ == "__main__":
             prefsnew[n, c] = rank
             for s in slots:
                 if compshortlists[c] > comppanels[c]:
-                    objcoeff[s, c, n] = (rank / (crit[n] + 1)) * (len(slots) + 1 - costs[s])
+                    objcoeff[s, c, n] = (rank / (crit[n])) * (len(slots) + 1 - costs[s])
                 else:
                     objcoeff[s, c, n] = (1 - rank / (crit[n] + 1)) * costs[s]
 
@@ -85,23 +114,27 @@ if __name__ == "__main__":
     choices = model.addVars(slots, clubs, names, vtype=GRB.BINARY, name='G')
 
     # Objective - allocate max students to the initial few slots
-    model.setObjective(
-        quicksum(
-            choices[s, c, n] * objcoeff.get((s, c, n), 1)
-            for s in slots for n in names for c in clubs),
-        GRB.MINIMIZE)
+    model.setObjective(quicksum(choices[s, c, n] * objcoeff.get((s, c, n), 1) for s in slots for n in names for c in clubs), GRB.MINIMIZE)
 
     # Constraint - maximum number in a slot for a club is limited by panels
     model.addConstrs((choices.sum(s, c, '*') <= panels[s, c] for s in slots for c in clubs))
 
     # Constraint - allocate student only if he has a shortlist
-    model.addConstrs((choices.sum('*', c, n) <= shortlists[n, c] for n in names for c in clubs))
+    model.addConstrs((choices.sum('*', c, n) <= shortlists[n, c] * slots_int.get(c, 1) for n in names for c in clubs))
 
     # Constraint - slots should not conflict for a student
     model.addConstrs((choices.sum(s, '*', n) <= 1 for s in slots for n in names))
 
     # Constraint - allocate all students or number of interviews possible
-    model.addConstrs((choices.sum('*', c, '*') == min(compshortlists[c], comppanels[c]) for c in clubs))
+    model.addConstrs((choices.sum('*', c, '*') == min(compshortlists[c], comppanels[c]) * slots_int.get(c, 1) for c in clubs))
+
+    # Constraint - for multiple slots per interview, same candidate should be allocated
+    for c, si in slots_int.items():
+        if si > 1:
+            for i in range(si - 1, len(slots), si):
+                for n in names:
+                    for j in range(i - si + 1, i):
+                        model.addConstr((choices[slots[i], c, n] - choices[slots[j], c, n]), GRB.EQUAL, 0)
 
     print('Optimising')
     model.optimize()
