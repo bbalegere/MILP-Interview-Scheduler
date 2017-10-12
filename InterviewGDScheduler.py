ce1127265b52ad13d7ff3377281801af3b4edcf4
bbalegere <bharatbalegere@gmail.com>
2017-10-12 18:49:39 +0530

    Handle staggered scheduling for GDs

--- InterviewGDScheduler.py
+++ InterviewGDScheduler.py
@@ -55,7 +55,19 @@ def read_shortlists(filename):
                 if len(n) > 1:
                     names.add(n)
                     shortlists[n, companies[i]] = 1
-    return shortlists, companies, names
+    return shortlists, companies, sorted(names)
+
+
+def read_gdPanels(filename):
+    gdcomps = set()
+    with open(filename) as f:
+        for csvline in f:
+            csvline = csvline.strip()
+            row = csvline.split(',')
+            val = [x for x in row if x]
+            gdcomps.add(tuple(val))
+
+    return gdcomps
 
 
 def usage():
@@ -64,7 +76,7 @@ def usage():
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, "hp:i:f:s:t:", ["help", "prefs=", "interviewslots=", "fixed=", "shortlist=", "time="])
+        opts, args = getopt.getopt(argv, "hi:f:s:t:g:", ["help", "interviewslots=", "fixed=", "shortlist=", "time=", "gdpanels="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -74,10 +86,9 @@ def main(argv):
     names = list()
     slots = list()
     panels = dict()
-    prefs = dict()
     slots_int = dict()
     fixedints = dict()
-
+    gdpanels = list()
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             usage()
@@ -85,15 +96,15 @@ def main(argv):
         elif opt in ("-s", "--shortlist"):
             shortlists, companies, names = read_shortlists(arg)
         elif opt in ("-t", "--time"):
-            panels, clubs2, slots = read_input_csv(arg)
-            print('Number of Clubs')
+            panels, comp2, slots = read_input_csv(arg)
+            print('Number of Companies')
             print(len(companies))
             print('Number of Candidates')
             print(len(names))
             print('Number of Slots')
             print(len(slots))
 
-            assert (sorted(companies) == sorted(clubs2))
+            assert (sorted(companies) == sorted(comp2))
 
             for val in shortlists.values():
                 if val not in [0, 1]:
@@ -106,23 +117,20 @@ def main(argv):
                 if val < 0:
                     raise ValueError('The number of panels cannot be negative')
 
-        elif opt in ("-p", "--prefs"):
-            prefs, clubs3, names2 = read_input_csv(arg)
-            assert (sorted(companies) == sorted(clubs3))
-
-            for val in prefs.values():
-                if val not in range(1, len(companies) + 1):
-                    raise ValueError('Incorrect preference ' + str(val) + '. It should be between 1 and ' + str(len(companies)))
-
         elif opt in ("-i", "--interviewslots"):
             slots_int = read_slots_interviews(arg)
+            assert (sorted(slots_int.keys()) == sorted(companies))
         elif opt in ("-f", "--fixed"):
             fixedints, clubs4, slots2 = read_input_csv(arg)
+        elif opt in ("-g", "--gdpanels"):
+            gdpanels = read_gdPanels(arg)
+            gdcomps = [y for x in gdpanels for y in x]
+            assert (sorted(companies) == sorted(gdcomps))
 
-    generateSchedule(companies, fixedints, names, panels, prefs, shortlists, slots, slots_int)
+    generateSchedule(companies, fixedints, names, panels, shortlists, slots, slots_int, gdpanels)
 
 
-def generateSchedule(companies, fixedints, names, panels, prefs, shortlists, slots, slots_int):
+def generateSchedule(companies, fixedints, names, panels, shortlists, slots, slots_int, gdpanels):
     print(datetime.now().time())
     # Find out max number of panels
     maxpanels = dict((c, max(panels[s, c] for s in slots)) for c in companies)
@@ -140,49 +148,38 @@ def generateSchedule(companies, fixedints, names, panels, prefs, shortlists, slo
     for c in companies:
         if compshortlists[c] > comppanels[c]:
             print(c + " has shortlists greater than no of panels " + str(compshortlists[c]) + " > " + str(comppanels[c]))
-    fibonacii = [1, 2]
-    for i in range(2, 1 + int(max(crit.values()))):
-        fibonacii.append(fibonacii[i - 1] + fibonacii[i - 2])
-
-    # Create Objective Coefficients
-    prefsnew = dict()
-    objcoeff = dict()
-    for n in names:
-        actpref = dict((c, prefs.get((n, c), 1) * shortlists.get((n, c), 0)) for c in companies if shortlists.get((n, c), 0) > 0)
-        scaledpref = {key: rank for rank, key in enumerate(sorted(actpref, key=actpref.get), 1)}
-
-        for c, rank in scaledpref.items():
-            prefsnew[n, c] = rank
-            for s in slots:
-                if compshortlists[c] > comppanels[c]:
-                    objcoeff[s, c, n] = (fibonacii[rank] / (crit[n])) * (len(slots) + 1 - costs[s])
-                else:
-                    objcoeff[s, c, n] = (1 - fibonacii[rank] / (crit[n] + 1)) * costs[s]
 
     print('Creating IPLP')
     model = Model('interviews')
     choices = model.addVars(slots, companies, names, vtype=GRB.BINARY, name='G')
     # Objective - allocate max students to the initial few slots
-    model.setObjective(quicksum(choices[s, c, n] * objcoeff.get((s, c, n), 1) for s in slots for n in names for c in companies), GRB.MINIMIZE)
+    model.setObjective(quicksum(choices[s, c, n] * costs[s] for s in slots for n in names for c in companies), GRB.MINIMIZE)
     # Constraint - maximum number in a slot for a club is limited by panels
     model.addConstrs((choices.sum(s, c, '*') <= panels[s, c] for s in slots for c in companies))
     # Constraint - allocate student only if he has a shortlist
-    model.addConstrs((choices.sum('*', c, n) <= shortlists.get((n, c), 0) * slots_int.get(c, 1) for n in names for c in companies))
+    model.addConstrs((choices.sum('*', c, n) <= shortlists.get((n, c[0]), 0) * slots_int.get(c[0], 1) for n in names for c in gdpanels))
     # Constraint - slots should not conflict for a student
     model.addConstrs((choices.sum(s, '*', n) <= 1 for s in slots for n in names))
     # Constraint - allocate all students or number of interviews possible
-    model.addConstrs((choices.sum('*', c, '*') == min(compshortlists[c], comppanels[c]) * slots_int.get(c, 1) for c in companies))
+    model.addConstrs((choices.sum('*', c, '*') == min(compshortlists[c[0]], comppanels[c[0]]) * slots_int.get(c[0], 1) for c in gdpanels))
     # Constraint - for multiple slots per interview, same candidate should be allocated
     for c, si in slots_int.items():
+
+        start_slot = 0
+        while panels[slots[start_slot], c] == 0:
+            start_slot += 1
+
         if si > 1:
-            for i in range(si - 1, len(slots), si):
+            for i in range(si - 1 + start_slot, len(slots), si):
                 for n in names:
                     for j in range(i - si + 1, i):
                         model.addConstr((choices[slots[i], c, n] - choices[slots[j], c, n]), GRB.EQUAL, 0)
+
     # Constraint - Fix manually given schedule
     for idx, n in fixedints.items():
         if n in names:
             model.addConstr(choices[idx[0], idx[1], n], GRB.EQUAL, 1)
+
     print('Optimising')
     model.optimize()
     solution = model.getAttr('X', choices)
@@ -199,7 +196,7 @@ def generateSchedule(companies, fixedints, names, panels, prefs, shortlists, slo
             i = 0
             for n in names:
                 if solution[s, c, n] == 1:
-                    row[i] = n + ' ' + str(int(prefsnew[n, c])) + '_' + str(int(crit[n]))
+                    row[i] = n
                     i = i + 1
 
             line = line + ',' + ','.join(row)
@@ -217,7 +214,7 @@ def generateSchedule(companies, fixedints, names, panels, prefs, shortlists, slo
             row = ''
             for c in companies:
                 if solution[s, c, n] == 1:
-                    row = c + '_' + str(int(prefsnew[n, c]))
+                    row = c
 
             line = line + ',' + row
 
