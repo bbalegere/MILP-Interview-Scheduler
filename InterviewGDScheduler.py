a4c2aa80af0fce7ba3f9b3f577384341e7675c1c
bbalegere <bharatbalegere@gmail.com>
2017-10-20 00:00:45 +0530

    Use Pandas and clean up code

--- InterviewGDScheduler.py
+++ InterviewGDScheduler.py
@@ -3,99 +3,64 @@ from datetime import datetime
 
 from gurobipy import *
 
+import pandas as pd
 
-def read_input_csv(filename):
-    row_header, matrix, col_header = list(), dict(), set()
-    with open(filename) as f:
-        for csvline in f:
-            csvline = csvline.strip()
-            if len(row_header) == 0:
-                row_header = csvline.split(',')
-                continue
-            row = csvline.split(',')
-            col_header.add(row[0])
-            for i in range(1, len(row)):
-                try:
-                    matrix[row[0], row_header[i]] = float(row[i])
-                except ValueError:
-                    matrix[row[0], row_header[i]] = row[i]
 
-    row_header.pop(0)
-    col_header = sorted(col_header)
-    return matrix, row_header, col_header
+def read_input_csv(filename, type=None):
+    sldf = pd.read_csv(filename, index_col=0, header=0, dtype=type)
+
+    return sldf.to_dict('index'), sorted(sldf.columns.values), list(sldf.index.values)
 
 
 def read_slots_interviews(filename):
-    row_header = list()
-    slots_c = list()
-    with open(filename) as f:
-        for csvline in f:
-            csvline = csvline.strip()
-            if len(row_header) == 0:
-                row_header = csvline.split(',')
-                continue
+    sidict = pd.read_csv(filename, dtype=object).to_dict('list')
 
-            slots_c = map(int, csvline.split(','))
-
-    return dict(zip(row_header, slots_c))
+    return dict((key, int(v[0])) for key, v in sidict.items())
 
 
 def read_shortlists(filename):
-    companies, shortlists, names = list(), dict(), set()
-    with open(filename) as f:
-        for csvline in f:
-            csvline = csvline.strip()
-            if len(companies) == 0:
-                companies = csvline.split(',')
-                continue
+    sldf = pd.read_csv(filename, dtype=object)
+    comtupl = tuplelist()
+    comps = list(sldf.columns.values)
+    for c in comps:
+        comtupl = comtupl + [(x, c) for x in list(sldf[c].dropna().values)]
 
-            row = csvline.split(',')
-            for i in range(len(row)):
-                n = row[i].strip()
-                if len(n) > 1:
-                    names.add(n)
-                    shortlists[n, companies[i]] = 1
-    return shortlists, companies, sorted(names)
+    return dict((x, 1) for x in comtupl), comps, sorted(set([x[0] for x in comtupl]))
 
 
 def read_gdPanels(filename):
-    gdcomps = set()
+    gdcomp = set()
     with open(filename) as f:
         for csvline in f:
-            csvline = csvline.strip()
-            row = csvline.split(',')
-            val = [x for x in row if x]
-            gdcomps.add(tuple(val))
+            gdcomp.add(tuple([x for x in csvline.strip().split(',') if x]))
 
-    return gdcomps
+    return gdcomp
 
 
 def read_lp(filename):
-    lp = set()
+    exnames = []
     with open(filename) as f:
         for csvline in f:
-            csvline = csvline.strip()
-            row = csvline.split(',')
-            lp.add(row[0])
+            exnames = exnames + [x for x in csvline.strip().split(',') if len(str(x).strip()) > 0]
 
-    return sorted(lp)
+    return sorted(set(exnames))
 
 
-def generateSchedule(companies, fixedints, names, panels, shortlists, slots, slots_int, gdpanels, lp):
+def generateSchedule(companies, fixedints, names, panels, shortlists, slots, slots_int, gdpanels):
     print(datetime.now().time())
     # Find out max number of panels
-    maxpanels = dict((c, max(panels[s, c] for s in slots)) for c in companies)
+    maxpanels = dict((c, max(panels[s][c] for s in slots)) for c in companies)
     # Generate cost of slots
     costs = dict((slots[s], s + 1) for s in range(len(slots)))
     # Calculate number shortlists for each students
     crit = dict((n, sum(shortlists.get((n, c), 0) for c in companies)) for n in names)
     # Remove names who dont have any shortlists
-    names = [key for key, value in crit.items() if value > 2 and key not in lp]
-    buffernames = [key for key, value in crit.items() if value <= 2 and key not in lp]
+    names = [key for key, value in crit.items() if value > 2]
+    buffernames = [key for key, value in crit.items() if value <= 2]
     # Calculate number shortlists per company
     compshortlists = dict((c, sum(shortlists.get((n, c), 0) for n in names)) for c in companies)
     # Calculate total number of panels per company
-    comppanels = dict((g[0], int(sum(panels[s, c] for s in slots for c in g) / slots_int.get(g[0], 1))) for g in gdpanels)
+    comppanels = dict((g[0], int(sum(panels[s][c] for s in slots for c in g) / slots_int.get(g[0], 1))) for g in gdpanels)
 
     for c in gdpanels:
         if compshortlists[c[0]] > comppanels[c[0]]:
@@ -107,7 +72,7 @@ def generateSchedule(companies, fixedints, names, panels, shortlists, slots, slo
     # Objective - allocate max students to the initial few slots
     model.setObjective(quicksum(choices[s, c, n] * costs[s] for s in slots for n in names for c in companies), GRB.MINIMIZE)
     # Constraint - maximum number in a slot for a club is limited by panels
-    model.addConstrs((choices.sum(s, c, '*') <= panels[s, c] for s in slots for c in companies))
+    model.addConstrs((choices.sum(s, c, '*') <= panels[s][c] for s in slots for c in companies))
     # Constraint - allocate student only if he has a shortlist
     model.addConstrs((choices.sum('*', c, n) <= shortlists.get((n, c[0]), 0) * slots_int.get(c[0], 1) for n in names for c in gdpanels))
     # Constraint - slots should not conflict for a student
@@ -118,7 +83,7 @@ def generateSchedule(companies, fixedints, names, panels, shortlists, slots, slo
     for c, si in slots_int.items():
 
         start_slot = 0
-        while panels[slots[start_slot], c] == 0:
+        while panels[slots[start_slot]][c] == 0:
             start_slot += 1
 
         if si > 1:
@@ -128,9 +93,10 @@ def generateSchedule(companies, fixedints, names, panels, shortlists, slots, slo
                         model.addConstr((choices[slots[i], c, n] - choices[slots[j], c, n]), GRB.EQUAL, 0)
 
     # Constraint - Fix manually given schedule
-    for idx, n in fixedints.items():
-        if n in names:
-            model.addConstr(choices[idx[0], idx[1], n], GRB.EQUAL, 1)
+    for idx, val in fixedints.items():
+        for idx2, n in val.items():
+            if n in names:
+                model.addConstr(choices[idx, idx2, n], GRB.EQUAL, 1)
 
     print('Optimising')
     model.optimize()
@@ -210,12 +176,13 @@ if __name__ == "__main__":
         if val not in [0, 1]:
             raise ValueError('The shortlists data can have only 0s or 1s indicating whether the student has a shortlist or not')
 
-    for val in panels.values():
-        if not val.is_integer():
-            raise ValueError('The number of panels should be a whole number')
+    for vall in panels.values():
+        for val in vall.values():
+            # if not val.is_integer():
+            #   raise ValueError('The number of panels should be a whole number')
 
-        if val < 0:
-            raise ValueError('The number of panels cannot be negative')
+            if val < 0:
+                raise ValueError('The number of panels cannot be negative')
 
     slots_int = read_slots_interviews(args.slotsgd)
     assert (sorted(slots_int.keys()) == sorted(companies))
@@ -226,10 +193,11 @@ if __name__ == "__main__":
 
     fixedints = dict()
     if args.fixed:
-        fixedints, clubs4, slots2 = read_input_csv(args.fixed)
+        fixedints, clubs4, slots2 = read_input_csv(args.fixed, type=object)
 
     lp = list()
     if args.leftprocess:
         lp = read_lp(args.leftprocess)
+        names = [n for n in names if n not in lp]
 
-    generateSchedule(companies, fixedints, names, panels, shortlists, slots, slots_int, gdpanels, lp)
+    generateSchedule(companies, fixedints, names, panels, shortlists, slots, slots_int, gdpanels)
